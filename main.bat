7c993e5b935f0a5beea129bafee274b2937f55a0
1mm0rt41PC <5358076+1mm0rt41PC@users.noreply.github.com>
2022-06-29 16:28:06 +0200

    Create main.bat

---
+++ main.bat
@@ -0,0 +1,16 @@
+CALL config.bat
+CALL pre-install.bat
+
+CALL build-py.bat rshell , rshell , 0
+
+:: Sync threading
+CALL sync-thread.bat 0
+:: #############################################################################
+:END_MAIN
+::7z a -t7z -mhe -p%_7Z_PASSWORD_% %_7Z_OUPUT_%\All.7z %scriptpath%\bin\*.exe
+::appveyor PushArtifact %_7Z_OUPUT_%\All.7z
+dir %scriptpath%\bin\
+dir %_7Z_OUPUT_%
+cd %_7Z_OUPUT_%
+CALL log.bat "✅ Build END"
+EXIT /B 0
